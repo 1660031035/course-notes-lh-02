9a011cf9c7f0ee8ad734b10d5d196f88c7fac0d4
1660031035 <102581655+1660031035@users.noreply.github.com>
2022-05-27 20:06:18 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 1. 配置axios 
+// 1.1 导入axios
+import axios from 'axios'
+// 2. 配置element-ui
+// 2.1 导入element-ui组件
+import ElementUi from 'element-ui'
+// 2.2 引入element-ui样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 1.2 把axios挂载到Vue的原型对象中
+Vue.prototype.$http = axios
+// 1.3 设置axios基地址
+axios.defaults.baseURL = 'http://www.liulongbin.top:3008'
 Vue.config.productionTip = false
-
+// 2.3 注册ElementUi
+Vue.use(ElementUi)
 new Vue({
   router,
   store,
